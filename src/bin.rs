823567a50ffb31374dd157df080a5c2485ebb095
Cantius Chew <cantiusc@blackmagicdesign.com>
2024-10-14 18:13:52 +0800

    Add rudimentary bin

--- src/bin.rs
+++ src/bin.rs
@@ -1,10 +1,48 @@
+use clap::{arg, command, Command};
 use dice::Dice;
 use rand::thread_rng;
 
 pub fn main() {
-    let x = "$220";
-    match x.parse::<Dice>() {
-        Ok(dice) => println!("{:?}", dice.roll(&mut thread_rng())),
-        Err(err) => println!("{}", err),
+    let matches = command!()
+        .subcommand(
+            Command::new("roll")
+                .about("Parse a dice script and roll it")
+                .arg(arg!(<SCRIPT> "Dice script to parse and roll"))
+                .arg(arg!(
+                    -d --debug "Turn debugging information on"
+                )),
+        )
+        .subcommand(
+            Command::new("seed")
+                .about("Seeds the prevailing random state")
+                .arg(arg!(<SEED> "Dice script to parse and roll"))
+                .arg(arg!(
+                    -d --debug "Turn debugging information on"
+                )),
+        )
+        .get_matches();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    if let Some(matches) = matches.subcommand_matches("roll") {
+        // You can check the value provided by positional arguments, or option arguments
+
+        if let Some(script) = matches.get_one::<String>("SCRIPT") {
+            // if matches.get_flag("debug") {
+            //     // "$ myapp test -l" was run
+            //     println!("debugging...");
+            // }
+            let result = script.parse::<Dice>();
+            match result {
+                Ok(dice) => {
+                    let result = dice.roll(&mut thread_rng());
+                    match result {
+                        Ok(result) => println!("{:?}", result),
+                        Err(err) => println!("Runtime Error: {}", err),
+                    }
+                }
+                Err(err) => println!("{}", err),
+            }
+        }
     }
 }
